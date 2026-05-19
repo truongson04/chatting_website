5d6e3072cde46c5f01c0a0049bcebc3baae22104
baodeptrai912 <dangson2376@gmail.com>
2026-05-19 16:48:14 +0700

    Build sign up feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,12 +8,13 @@ import ProfilePage from "./components/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
   useEffect(() => {
     checkAuth();
-  }, []);
+  }, [checkAuth]);
   if (isCheckingAuth && !authUser) {
     return (
       <div className="flex items-center justify-center h-screen">
@@ -43,6 +44,7 @@ export default function App() {
           element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
         />
       </Routes>
+      <Toaster />
     </>
   );
 }
--- frontend/src/store/useAuthStore.tsx
+++ frontend/src/store/useAuthStore.tsx
@@ -1,5 +1,8 @@
 import { create } from "zustand";
 import clientApi from "../lib/axios";
+import type { User } from "../components/SignUpPage";
+import { toast } from "react-hot-toast";
+import axios from "axios";
 interface AuthState {
   authUser: any;
   isCheckingAuth: boolean;
@@ -7,7 +10,9 @@ interface AuthState {
   isLoggingIn: boolean;
   isUpdatingProfile: boolean;
   checkAuth: () => void;
+  signUp: (formData: User) => void;
 }
+
 export const useAuthStore = create<AuthState>()((set) => ({
   authUser: null,
   isCheckingAuth: true,
@@ -23,6 +28,25 @@ export const useAuthStore = create<AuthState>()((set) => ({
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
+    }
+  },
+
+  signUp: async (formData: User) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await clientApi.post("/auth/signUp", formData);
+      toast.success("Sign up successfully");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log(error);
+      // kiểm tra lỗi bằng axios
+      if (axios.isAxiosError(error)) {
+        toast.error(error.response?.data.message);
+      } else if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    } finally {
+      set({ isSigningUp: false });
     }
   },
 }));
